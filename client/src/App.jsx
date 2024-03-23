4d95d7a5ff6320ae287c9f7b74e606d71a2ee1a1
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-23 09:11:52 +0000

    Set up frontend with vite and tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red-800'>
+        Hello Video Platform
+       </div>
+       <p className='text-2xl'>
+        Come here
+       </p>
+    </>
+  )
+}
+
+export default App
