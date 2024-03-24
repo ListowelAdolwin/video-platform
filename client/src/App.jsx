526d483f6871af2ce42213e4d3a89972a72af4db
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-24 07:27:52 +0000

    Set up routes in client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import VerifyEmail from "./pages/VerifyEmail";
+import Video from "./pages/Video";
 
 function App() {
-
   return (
-    <>
-      <div className='text-red-800'>
-        Hello Video Platform
-       </div>
-       <p className='text-2xl'>
-        Come here
-       </p>
-    </>
-  )
+    <div className="bg-gray-700 h-screen">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify-email/:token" element={<VerifyEmail />} />
+          <Route path="/reset-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="/video/:id" element={<Video />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
