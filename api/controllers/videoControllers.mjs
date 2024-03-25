676df6d451a7273d527de4e39446ea5eb8a26552
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-25 17:40:17 +0000

    Created endpoints to delete and get next video

--- api/controllers/videoControllers.mjs
+++ api/controllers/videoControllers.mjs
@@ -9,11 +9,20 @@ export const saveVideo = async (req, res, next) => {
     }
 
     try {
+        const lastVideo = await Video.findOne().sort({ createdAt: -1 })
+        console.log("Last vid: ", lastVideo)
         const newVideo = await Video.create({
             title,
             description,
-            videoUrl
+            videoUrl,
+            prevVid: lastVideo ? lastVideo._id : null
         })
+        if (lastVideo) {
+            await Video.findByIdAndUpdate(lastVideo._id, {
+            nextVid: newVideo._id,
+      });
+    }
+        
         res.status(201).json({ok: true, msg: "Video saved successfully", video: newVideo})
     } catch (error) {
         console.log(error)
@@ -29,5 +38,63 @@ export const getVideos = async (req, res) => {
     } catch (error) {
         res.status(500).json({ok: false, msg: "Error getting video"})
         console.log(error)
+    }
+}
+
+export const getVideo = async (req, res) => {
+    const id = req.params.id
+    try {
+        const video = await Video.findById(id)
+        res.json({ok: true, msg: "Videos fetched successfully", video})
+    } catch (error) {
+        res.status(500).json({ok: false, msg: "Error getting video"})
+        console.log(error)
+    }
+}
+
+export const getNextVideo = async (req, res) => {
+    const id = req.params.id
+    
+    console.log("Next vid: ", nextVid)
+    try {
+        const vid = await Video.findById(id)
+        const nextVid = vid.nextVid
+        res.json({ok: true, msg: "Videos fetched successfully", nextVid})
+    } catch (error) {
+        res.status(500).json({ok: false, msg: "Error getting video"})
+        console.log(error)
+    }
+}
+
+
+export const deleteVideo = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const video = await Video.findById(id)
+        console.log("video: ", video)
+        if (!video) {
+            return res.status(404).json({msg: "Video not found", ok: false})
+        }
+        const prevVid = await Video.findById(video.prevVid)
+        const nextVid = await Video.findById(video.nextVid)
+        console.log("prev: ", prevVid)
+        console.log("next: ", nextVid)
+        if (prevVid){
+            prevVid.nextVid = nextVid?.id || null
+            await prevVid.save()
+            }
+        
+        if (nextVid){
+            nextVid.prevVid = prevVid?.id || null
+            await nextVid.save()
+        }
+         
+        await Video.findByIdAndDelete(id)
+        res.status(200).json({msg: "Video deleted", ok:true})
+        
+    } catch (error) {
+        console.log("Error in video deletion: ", error)
+        return res.json({msg: "Could not delete video", ok: false})
     }
 }
--- api/routes/videoRoutes.mjs
+++ api/routes/videoRoutes.mjs
@@ -1,9 +1,12 @@
 import express from "express";
-import { saveVideo, getVideos } from "../controllers/videoControllers.mjs";
+import { saveVideo, getVideos, getVideo, getNextVideo, deleteVideo } from "../controllers/videoControllers.mjs";
 
 const router = express.Router()
 
 router.get("", getVideos)
+router.get("/:id", getVideo)
 router.post("/save", saveVideo)
+router.post("/next", getNextVideo)
+router.get("/delete/:id", deleteVideo)
 
 export default router;
