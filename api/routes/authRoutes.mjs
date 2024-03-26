703ac37ee06633a9b55ef5d8857ff9c2e31365c7
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-26 16:57:12 +0000

    Improved formatting

--- api/routes/authRoutes.mjs
+++ api/routes/authRoutes.mjs
@@ -1,18 +1,27 @@
-import express from "express"
-import { loginUser, logoutUser, refreshToken, registerUser, resendEmail, resetPassword, verifyEmail, sendPasswordResetEmail, verifyToken } from "../controllers/authControllers.mjs";
+import express from 'express'
+import {
+  loginUser,
+  logoutUser,
+  refreshToken,
+  registerUser,
+  resendEmail,
+  resetPassword,
+  verifyEmail,
+  sendPasswordResetEmail,
+  verifyToken
+} from '../controllers/authControllers.mjs'
 
 const router = express.Router()
 
-router.post("/register", registerUser)
-router.post("/login", loginUser)
+router.post('/register', registerUser)
+router.post('/login', loginUser)
 router.get('/logout', logoutUser)
 router.get('/verify-email/:token', verifyEmail)
 // router.post('/validate-token', reactValidateToken)
-router.get('/resend-email/:token', resendEmail)
+router.get('/resend-email/:id', resendEmail)
 router.post('/reset-password/:token', resetPassword)
 router.post('/reset-password', sendPasswordResetEmail)
 router.post('/refresh', refreshToken)
 // router.post('/delete-user', deleteUser)
-
 
 export default router
--- api/routes/userRoutes.mjs
+++ api/routes/userRoutes.mjs
@@ -1,10 +1,9 @@
-import express from "express";
-import { getUsers } from "../controllers/userControllers.mjs";
-import { verifyToken } from "../controllers/authControllers.mjs";
+import express from 'express'
+import { getUsers } from '../controllers/userControllers.mjs'
+import { verifyToken } from '../controllers/authControllers.mjs'
 
 const router = express.Router()
 
-router.get("/", verifyToken, getUsers)
-
+router.get('/', verifyToken, getUsers)
 
 export default router
