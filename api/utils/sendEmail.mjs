3e7083619a34ae71c57c2aef52aa1645f482fb81
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-26 16:56:12 +0000

    Improved the email templates for password reset and email verification

--- api/utils/sendEmail.mjs
+++ api/utils/sendEmail.mjs
@@ -1,34 +1,42 @@
 import mailer from 'nodemailer'
 
-
 const sendCustomEmail = (from, to, subject, emailVerifyToken) => {
-    const transporter = mailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.APP_EMAIL,
-            pass: process.env.APP_PASSWORD,
-        }
-    })
+  const transporter = mailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.APP_EMAIL,
+      pass: process.env.APP_PASSWORD
+    }
+  })
 
-    const html = `<h3>Hello there friend from Listo, please the next step after the registering is to confirm your email so we are sure you did not by mistake enter the wrong email </br> \
-    Please click the link below to verify </br> \
-    <a href="http://127.0.0.1:5173/verify-email/${emailVerifyToken}">verify email</a> \
-    </br> </br>\
-    You've got this! </h3>`
+  const html = `<h3>Hello there friend from Listo,</h3>
+  <p>
+    Please confirm your email address to complete your registration and ensure you didn't enter it incorrectly.
+  </p>
+  <p>
+    Click the link below to verify your email:
+  </p>
+  <a href="http://127.0.0.1:5173/verify-email/${emailVerifyToken}">Verify Email</a>
+  <p>
+    You've got this!
+  </p>`
 
-    transporter.sendMail({
-        from,
-        to,
-        subject,
-        html
-    }, (err, info) => {
-        if (err) {
-            console.log(`Failed to send email to ${to}`)
-            console.log(err)
-        } else {
-            console.log(`Email sent to ${to}`)
-        }
-    })
+  transporter.sendMail(
+    {
+      from,
+      to,
+      subject,
+      html
+    },
+    (err, info) => {
+      if (err) {
+        console.log(`Failed to send email to ${to}`)
+        console.log(err)
+      } else {
+        console.log(`Email sent to ${to}`)
+      }
+    }
+  )
 }
 
 export default sendCustomEmail
