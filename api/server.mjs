1303605d1a152659cef8f14c2e391edb93e5b3a1
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-27 23:53:26 +0000

    Made deployment configurations for the backend

--- api/server.mjs
+++ api/server.mjs
@@ -1,25 +1,39 @@
-import express from "express"
-import mongoose from "mongoose"
+import express from 'express'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import connectDB from "./config/db.mjs"
-import authRoutes from "./routes/authRoutes.mjs"
-import userRoutes from "./routes/userRoutes.mjs"
-import videoRoutes from "./routes/videoRoutes.mjs"
+import connectDB from './config/db.mjs'
+import authRoutes from './routes/authRoutes.mjs'
+import userRoutes from './routes/userRoutes.mjs'
+import videoRoutes from './routes/videoRoutes.mjs'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 
+app.use(
+  cors({
+    origin: [
+      'https://video-platform-client.onrender.com',
+      'http://127.0.0.1:5173/',
+      'http://video-platform-client.onrender.com',
+      'https://127.0.0.1:5173/'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  })
+)
+
 app.use(express.json())
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/videos", videoRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/videos', videoRoutes)
 
 connectDB()
 
 mongoose.connection.once('open', () => {
-    console.log('DB connected')
-    app.listen(3000, () => {
-    console.log("App started")
-    })
+  console.log('DB connected')
+  app.listen(3000, () => {
+    console.log('App started')
+  })
 })
