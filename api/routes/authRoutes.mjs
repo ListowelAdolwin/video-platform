40a3acd45273c3475ecd97bcefe8631b24fa71b8
ListowelAdolwin <listoweladolwin@gmail.com>
2024-05-03 09:25:30 +0000

    Implemented refresh tokens in server

--- api/routes/authRoutes.mjs
+++ api/routes/authRoutes.mjs
@@ -1,27 +1,27 @@
-import express from 'express'
+import express from "express";
 import {
-  loginUser,
-  logoutUser,
-  refreshToken,
-  registerUser,
-  resendEmail,
-  resetPassword,
-  verifyEmail,
-  sendPasswordResetEmail,
-  verifyToken
-} from '../controllers/authControllers.mjs'
+	loginUser,
+	logoutUser,
+	refreshToken,
+	registerUser,
+	resendEmail,
+	resetPassword,
+	verifyEmail,
+	sendPasswordResetEmail,
+	verifyAccessToken,
+} from "../controllers/authControllers.mjs";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/register', registerUser)
-router.post('/login', loginUser)
-router.get('/logout', logoutUser)
-router.get('/verify-email/:token', verifyEmail)
-// router.post('/validate-token', reactValidateToken)
-router.get('/resend-email/:id', resendEmail)
-router.post('/reset-password/:token', resetPassword)
-router.post('/reset-password', sendPasswordResetEmail)
-router.post('/refresh', refreshToken)
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logoutUser);
+router.get("/verify-email/:token", verifyEmail);
+router.get("/token-validity", verifyAccessToken);
+router.get("/resend-email/:id", resendEmail);
+router.post("/reset-password/:token", resetPassword);
+router.post("/reset-password", sendPasswordResetEmail);
+router.get("/refresh", refreshToken);
 // router.post('/delete-user', deleteUser)
 
-export default router
+export default router;
