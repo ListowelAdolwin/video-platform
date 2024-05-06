d1df31e27403daaad7fdb8cbac46ec22e71a5a73
ListowelAdolwin <listoweladolwin@gmail.com>
2024-05-06 17:53:38 +0000

    Added tooltips to buttons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -49,12 +49,17 @@ function HeaderNew() {
 								Hi, {currentUser.username}
 							</span>
 							{currentUser?.isAdmin && (
-								<Link
-									className="text-white items-center justify-center rounded-xl bg-slate-800 px-3 py-2 text-sm font-semibold shadow-sm transition-all duration-150 hover:outline-3 hover:ring-1 hover:ring-offset-1 hover:opacity-85"
-									to="/upload"
-								>
-									Upload
-								</Link>
+								<div className="group relative flex justify-center">
+									<Link
+										className="text-white items-center justify-center rounded-xl bg-slate-800 px-3 py-2 text-sm font-semibold shadow-sm transition-all duration-150 hover:outline-3 hover:ring-1 hover:ring-offset-1 hover:opacity-85"
+										to="/upload"
+									>
+										Upload
+									</Link>
+									<span className="text-bold text-center absolute top-10 scale-0 transition-all rounded bg-gray-700 p-2 text-xs text-white group-hover:scale-100 w-24">
+										Upload video
+									</span>
+								</div>
 							)}
 							{isLoading ? (
 								<div className="">
@@ -70,29 +75,45 @@ function HeaderNew() {
 									/>
 								</div>
 							) : (
-								<button
-									type="button"
-									onClick={handleLogout}
-									className="text-white items-center justify-center rounded-xl bg-slate-800 px-3 py-2 text-sm font-semibold shadow-sm transition-all duration-150 hover:opacity-95"
-								>
-									<IoIosLogOut />
-								</button>
+								<div className="group relative flex justify-center">
+									<button
+										type="button"
+										onClick={handleLogout}
+										className="text-white items-center justify-center rounded-xl bg-slate-800 px-3 py-2 text-sm font-semibold shadow-sm transition-all duration-150 hover:opacity-95"
+									>
+										<IoIosLogOut />
+									</button>
+									<span className="text-bold text-center absolute top-10 scale-0 transition-all rounded bg-gray-700 p-2 text-xs text-white group-hover:scale-100 w-auto">
+										Logout
+									</span>
+								</div>
 							)}
 						</div>
 					) : (
 						<div className="flex items-center justify-end gap-3">
-							<Link
-								className="text-white items-center justify-center rounded-xl bg-slate-800 px-3 py-2 text-sm font-semibold shadow-sm transition-all duration-150 hover:outline-3 hover:ring-1 hover:ring-offset-1 hover:opacity-85"
-								to="/login"
-							>
-								Sign in
-							</Link>
-							<Link
-								className="inline-flex items-center justify-center rounded-xl bg-slate-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:outline-3 hover:ring-1 hover:ring-offset-1 hover:opacity-85"
-								to="/register"
-							>
-								Sign up
-							</Link>
+							<div className="group relative flex justify-center">
+								<Link
+									className="text-white items-center justify-center rounded-xl bg-slate-800 px-3 py-2 text-sm font-semibold shadow-sm transition-all duration-150 hover:outline-3 hover:ring-1 hover:ring-offset-1 hover:opacity-85"
+									to="/login"
+								>
+									Sign in
+								</Link>
+								<span className="text-bold text-center absolute top-10 scale-0 transition-all rounded bg-gray-700 p-2 text-xs text-white group-hover:scale-100 w-16">
+									Sign in
+								</span>
+							</div>
+
+							<div className="group relative flex justify-center">
+								<Link
+									className="inline-flex items-center justify-center rounded-xl bg-slate-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:outline-3 hover:ring-1 hover:ring-offset-1 hover:opacity-85"
+									to="/register"
+								>
+									Sign up
+								</Link>
+								<span className="text-bold text-center absolute top-10 scale-0 transition-all rounded bg-gray-700 p-2 text-xs text-white group-hover:scale-100 w-16">
+									Sign up
+								</span>
+							</div>
 						</div>
 					)}
 				</div>
